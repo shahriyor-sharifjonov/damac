db0918aa5678438499bba4283f1a319c627d9fa4
Shahriyor <shahriyordeveloping@gmail.com>
2023-01-11 10:30:19 +0500

    end

--- src/js/app.js
+++ src/js/app.js
@@ -32,7 +32,7 @@ document.querySelectorAll('.amenities__item').forEach(el => {
     el.classList.remove('visible');
   }, {
     root: null,
-    threshold: 0.9,
+    threshold: 0.7,
   })
   observer.observe(el);
 })
