df5902fc78c89ec06247b794d7c0e6120af0c740
Shahriyor <shahriyordeveloping@gmail.com>
2023-01-12 11:31:12 +0500

    popup

--- src/js/modules/popup.js
+++ src/js/modules/popup.js
@@ -9,19 +9,37 @@ function close() {
     })
 }
 
-const el = document.querySelector('body').getBoundingClientRect(); //234,66 235
-console.log(el.height);
-
 export function init() {
     buttons.forEach(button => {
         button.addEventListener('click', () => {
             const target = button.getAttribute('data-popup');
             const popup = document.querySelector(target);
-            console.log(target);
             popup.classList.add('active');
             document.body.style.overflowY = 'hidden';
         }); 
     });
+
+    popups.forEach(popup => {
+        const bg = popup.querySelector('.popup__bg');
+        if(bg.classList.contains('multiple')){
+            console.log('multiple detected');
+            function loop(){
+                console.log('starting loop function');
+                const img = popup.querySelector('.popup__bg-img.active');
+                const firstImg = popup.querySelector('.popup__bg-img.first');
+                setTimeout(() => {
+                    img.classList.remove('active');
+                    if(!img.classList.contains('last')){
+                        img.nextElementSibling.classList.add('active');
+                    }else{
+                        firstImg.classList.add('active');
+                    }
+                    loop()
+                }, 2000);
+            }
+            loop();
+        }
+    })
 
     closeButtons.forEach(closeButton => {
         closeButton.addEventListener('click', () => {
