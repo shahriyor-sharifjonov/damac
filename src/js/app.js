eabc161dfc057a2a1825bf07f90edb0a10a0d703
Shahriyor <shahriyordeveloping@gmail.com>
2023-01-13 19:02:18 +0500

    init

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,48 @@
+import gsap from 'gsap'
+
+const preloader = document.querySelector('#preloader');
+const loaderImgs = document.querySelectorAll('.loader-img')
+const body = document.body;
+
+const tlpre = gsap.timeline();
+
+tlpre.to(preloader, { duration: 0.5, });
+tlpre.to('#loader-title', { opacity: 1, duration: 0.2 });
+
+loaderImgs.forEach(item => {
+
+    tlpre.to(item, {
+        className: "loader-img active",
+        duration: 1,
+        ease: "power3.ease",
+    });
+
+    tlpre.set(item, {
+        className: "loader-img leave",
+        delay: 0.5,
+        duration: 1,
+        ease: "power3.ease",
+    })
+
+
+    if (window.innerWidth >= 460) {
+
+    }
+
+})
+tlpre.to('#loader-title', { xPercent: -190, scale: 80, duration: 0.2, });
+tlpre.to(preloader, { backgroundColor: "#fff", opacity: 0, delay: 0.1, });
+tlpre.to(preloader, { yPercent: -100, delay: 0.2, });
+tlpre.to(body, { className: 'loaded' });
+
+
+window.addEventListener('load', function (event) {
+    // document.body.classList.add('loaded');
+});
+
+
+
+
 // import * as smoothscroll from "./modules/smoothscroll.js";
 import * as locationmap from "./modules/locationmap.js";
 import * as functions from "./modules/functions.js";
@@ -26,3 +71,4 @@ functions.showmore();
 amenities.init();
 floorpopup.init();
 header.init();
+
